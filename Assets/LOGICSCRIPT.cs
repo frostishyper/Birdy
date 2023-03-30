c6d84db333bc1bfae04cc5a22dab732f0b18d5aa
frostishyper <71824443+frostishyper@users.noreply.github.com>
2023-03-30 11:24:34 +0800

    Add files via upload
    Alpha 0.1

---
+++ Assets/LOGICSCRIPT.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LOGICSCRIPT : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+    
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd) {
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+    public  void Retry() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void gameOver() {
+        gameOverScreen.SetActive(true);
+    }
+}
